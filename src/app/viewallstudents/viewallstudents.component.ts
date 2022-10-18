a1f1270f6f1bf9f83979b3fffea5e8f519717637
Elda-Dani <eldadani2001@gmail.com>
2022-10-18 23:56:48 +0530

    edit

--- src/app/viewallstudents/viewallstudents.component.ts
+++ src/app/viewallstudents/viewallstudents.component.ts
@@ -16,7 +16,9 @@ export class ViewallstudentsComponent implements OnInit {
     {"name":"Leena","roll":"05","admission":"2544","college":"newman college","department":"computer science","bloodgroup":"AB+ve","dob":"1999-12-12","parentname":"Xavier","parentemail":"xavier@gmail.com","guardianname":"Xavier","guardianaddress":"abc"},
     {"name":"Lara","roll":"04","admission":"2545","college":"nirmala college","department":"computer science","bloodgroup":"A+ve","dob":"2001-01-14","parentname":"Joy","parentemail":"joy@gmail.com","guardianname":"Joy","guardianaddress":"xyzc"},
     {"name":"Hari","roll":"07","admission":"2546","college":"uc college","department":"mechanical engineering","bloodgroup":"B+ve","dob":"1998-01-01","parentname":"Sankar","parentemail":"san12@gmail.com","guardianname":"Sankar","guardianaddress":"abcd"},
-   
+    {"name":"Meera","roll":"08","admission":"2547","college":"Christ college","department":"civil engineering","bloodgroup":"B-ve","dob":"2000-10-11","parentname":"Manoj","parentemail":"manu@gmail.com","guardianname":"manoj","guardianaddress":"prqs"},
+    {"name":"Anna","roll":"03","admission":"2542","college":"santhigiri college","department":"Electrical engineering","bloodgroup":"A+ve","dob":"1999-03-31","parentname":"George","parentemail":"geo23@gmail.com","guardianname":"george","guardianaddress":"qwert"},
+
   ]
 
 
--- src/app/viewfaculty/viewfaculty.component.ts
+++ src/app/viewfaculty/viewfaculty.component.ts
@@ -15,6 +15,8 @@ facultyData:any=[
   {"name":"Mathew","department":"Computer Science","designation":"assistant professor","dob":"1988-12-21","education":"Mca","address":"xyz","mobile":"9875883218","datejoin":"2018-06-08"},
   {"name":"Mini","department":"Electrical engineering","designation":"assistant professor","dob":"1975-08-12","education":"Mtech","address":"qwert","mobile":"9425463218","datejoin":"2015-05-03"},
   {"name":"Archana","department":"Civil engineering","designation":"assistant professor","dob":"1995-09-24","education":"Msw","address":"pqrs","mobile":"7975463218","datejoin":"2022-01-31"},
+  {"name":"Reji","department":"Mechanical engineering","designation":"assistant professor","dob":"1975-05-30","education":"MA","address":"abcds","mobile":"7975463789","datejoin":"2019-04-01"},
+  {"name":"Theresa","department":"Computer Science","designation":"assistant professor","dob":"1993-03-18","education":"Mca","address":"asdff","mobile":"7977563218","datejoin":"2020-06-02"},
 
 ]
 
